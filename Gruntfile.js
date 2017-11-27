79e08beaa8a369bc52cc5f95a55dec7407f87705
Trevor Roman <troman360@gmail.com>
2017-11-26 23:02:12 -0500

    Initial build configuration

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  var config = grunt.file.readYAML('Gruntconfig.yml');
+  var pkg = grunt.file.readJSON('package.json');
+
+  grunt.initConfig({
+    clean: ['build', 'dist'],
+    jshint: {
+      files: ['Gruntfile.js', config.jsSrcDir + '**/*.js'],
+    },
+    concat: {
+      scss: {
+        src: [config.scssSrcDir + '**/*.scss'],
+        dest: config.scssBuildDir + pkg.name + '.scss'
+      },
+      js: {
+        src: [config.jsSrcDir + '**/*.js'],
+        dest: config.jsBuildDir + pkg.name + '.js'
+      }
+    },
+    htmlmin: {
+      options: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      dist: {
+        expand: true,
+        cwd: config.htmlSrcDir,
+        src: '**/*.html',
+        dest: config.htmlDistDir,
+      }
+    },
+    sass: {
+      dist: {
+        src: config.scssBuildDir + pkg.name + '.scss',
+        dest: config.cssDistDir + pkg.name + '.css'
+      }
+    },
+    uglify: {
+      dist: {
+        src: config.jsBuildDir + pkg.name + '.js',
+        dest: config.jsDistDir + pkg.name + '.js'
+      }
+    },
+    watch: {
+      scripts: {
+        files: [config.jsSrcDir + '**/*.js', config.htmlSrcDir + '**/*.html',
+          config.scssSrcDir + '**/*.scss'
+        ],
+        tasks: ['clean', 'jshint', 'concat', 'htmlmin',
+          'sass', 'uglify'
+        ],
+        options: {
+          spawn: false,
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean', 'jshint', 'concat', 'htmlmin',
+    'sass', 'uglify', 'watch'
+  ]);
+};
